5852a7c72ed3cb24d29527533c231f84f97e7569
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-07 20:26:31 +0900

    Minimal absolutely rendered rounded selection model

--- src/App.js
+++ src/App.js
@@ -1,91 +1,170 @@
+/* eslint-disable no-unused-vars */
+
 import React from "react"
 import styled from "styled-components"
 
-const Relative = styled.div`
-	position: relative;
-`
+// const Selection = styled.span`
+// 	display: inline-block;
+// 	background-color: hsl(195, 100%, 90%);
+// 	border-top-left-radius: ${props => (!props.topLeft && !props.left ? 0 : "6px")};
+// 	border-top-right-radius: ${props => (!props.topRight && !props.right ? 0 : "6px")};
+// 	border-bottom-left-radius: ${props => (!props.bottomLeft && !props.left ? 0 : "6px")};
+// 	border-bottom-right-radius: ${props => (!props.bottomRight && !props.right ? 0 : "6px")};
+// `
 
-const AbsoluteSelectionWrapper = styled.div`
-	position: absolute;
-	pointer-events: none;
-	user-select: none;
+const Center = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
 `
-const HiddenSelection = styled.span`
-	visibility: hidden;
-`
-const ActiveSelection = styled.span`
-	display: inline-block;
-	background-color: hsl(195, 100%, 90%);
-	border-top-left-radius: ${props => (!props.topLeft && !props.left ? 0 : "6px")};
-	border-top-right-radius: ${props => (!props.topRight && !props.right ? 0 : "6px")};
-	border-bottom-left-radius: ${props => (!props.bottomLeft && !props.left ? 0 : "6px")};
-	border-bottom-right-radius: ${props => (!props.bottomRight && !props.right ? 0 : "6px")};
+const InsetPadding = styled.div`
+	padding: 96px 24px;
 `
 
 export default function App() {
 	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "row",
-				justifyContent: "center",
-			}}
-		>
-			<div style={{ padding: "96px 24px" }}>
+		<Center>
+			<InsetPadding style={{ width: "100%", maxWidth: 768 }}>
 				<article>
 					{/**/}
 
-					<Relative>
-						<AbsoluteSelectionWrapper>
-							<HiddenSelection>Occaecat ad&nbsp;</HiddenSelection>
-							<ActiveSelection topLeft topRight bottomRight>
-								officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
-							</ActiveSelection>
-						</AbsoluteSelectionWrapper>
-						Occaecat ad officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
-					</Relative>
-					<Relative>
-						<AbsoluteSelectionWrapper>
-							<ActiveSelection topLeft>
-								ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum.
-								Proident&nbsp;
-							</ActiveSelection>
-						</AbsoluteSelectionWrapper>
-						ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum. Proident&nbsp;
-					</Relative>
-					<Relative>
-						<AbsoluteSelectionWrapper>
-							<ActiveSelection topRight>
-								nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
-								officia&nbsp;
-							</ActiveSelection>
-						</AbsoluteSelectionWrapper>
-						nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
-						officia&nbsp;
-					</Relative>
-					<Relative>
-						<AbsoluteSelectionWrapper>
-							<ActiveSelection right>
-								fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
-								minim&nbsp;
-							</ActiveSelection>
-						</AbsoluteSelectionWrapper>
-						fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
-						minim&nbsp;
-					</Relative>
-					<Relative>
-						<AbsoluteSelectionWrapper>
-							<ActiveSelection bottomLeft bottomRight>
-								laborum non aliqua exercitation sunt ad mollit&nbsp;
-							</ActiveSelection>
-							<HiddenSelection>pariatur ea.&nbsp;</HiddenSelection>
-						</AbsoluteSelectionWrapper>
-						laborum non aliqua exercitation sunt ad mollit pariatur ea.&nbsp;
-					</Relative>
+					<div
+						style={{
+							position: "relative",
+							height: 28,
+						}}
+					>
+						<div
+							style={{
+								display: "inline-block",
+								width: "60%",
+								height: "100%",
+								backgroundColor: "hsl(195, 100%, 90%)",
+								borderTopLeftRadius: 6,
+								borderTopRightRadius: 6,
+								borderBottomRightRadius: 6,
+							}}
+						/>
+					</div>
+					<div
+						style={{
+							position: "relative",
+							height: 28,
+						}}
+					>
+						<div
+							style={{
+								display: "inline-block",
+								width: "50%",
+								height: "100%",
+								backgroundColor: "hsl(195, 100%, 90%)",
+							}}
+						/>
+						<div
+							style={{
+								position: "relative",
+								display: "inline-block",
+								height: "100%",
+							}}
+						>
+							<div
+								style={{
+									position: "absolute",
+									top: 0,
+									bottom: 0,
+								}}
+							>
+								<div
+									style={{
+										display: "inline-block",
+										width: 28,
+										height: "100%",
+										backgroundColor: "hsl(0, 0%, 100%)",
+										borderTopLeftRadius: 6,
+										borderBottomLeftRadius: 6,
+									}}
+								/>
+							</div>
+							<div
+								style={{
+									display: "inline-block",
+									width: 28,
+									height: "100%",
+									backgroundColor: "hsl(195, 100%, 90%)",
+								}}
+							/>
+						</div>
+					</div>
+					<div
+						style={{
+							position: "relative",
+							height: 28,
+						}}
+					>
+						<div
+							style={{
+								display: "inline-block",
+								width: "60%",
+								height: "100%",
+								backgroundColor: "hsl(195, 100%, 90%)",
+								borderTopRightRadius: 6,
+								borderBottomLeftRadius: 6,
+								borderBottomRightRadius: 6,
+							}}
+						/>
+					</div>
 
 					{/**/}
 				</article>
-			</div>
-		</div>
+			</InsetPadding>
+		</Center>
 	)
 }
+
+// <Relative>
+// 	<AbsoluteSelectionWrapper>
+// 		<HiddenSelection>Occaecat ad&nbsp;</HiddenSelection>
+// 		<ActiveSelection topLeft topRight bottomRight>
+// 			officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
+// 		</ActiveSelection>
+// 	</AbsoluteSelectionWrapper>
+// 	Occaecat ad officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
+// </Relative>
+// <Relative>
+// 	<AbsoluteSelectionWrapper>
+// 		<ActiveSelection topLeft>
+// 			ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum.
+// 			Proident&nbsp;
+// 		</ActiveSelection>
+// 	</AbsoluteSelectionWrapper>
+// 	ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum. Proident&nbsp;
+// </Relative>
+// <Relative>
+// 	<AbsoluteSelectionWrapper>
+// 		<ActiveSelection topRight>
+// 			nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
+// 			officia&nbsp;
+// 		</ActiveSelection>
+// 	</AbsoluteSelectionWrapper>
+// 	nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
+// 	officia&nbsp;
+// </Relative>
+// <Relative>
+// 	<AbsoluteSelectionWrapper>
+// 		<ActiveSelection right>
+// 			fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
+// 			minim&nbsp;
+// 		</ActiveSelection>
+// 	</AbsoluteSelectionWrapper>
+// 	fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
+// 	minim&nbsp;
+// </Relative>
+// <Relative>
+// 	<AbsoluteSelectionWrapper>
+// 		<ActiveSelection bottomLeft bottomRight>
+// 			laborum non aliqua exercitation sunt ad mollit&nbsp;
+// 		</ActiveSelection>
+// 		<HiddenSelection>pariatur ea.&nbsp;</HiddenSelection>
+// 	</AbsoluteSelectionWrapper>
+// 	laborum non aliqua exercitation sunt ad mollit pariatur ea.&nbsp;
+// </Relative>
