af95f7b5cf831bc6ce7b1c2a1c69185228aea486
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-07 21:02:29 +0900

    Added inactive states for the cursor and selection

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@
 import React from "react"
 import styled from "styled-components"
 
-// const Selection = styled.span`
-// 	display: inline-block;
-// 	background-color: hsl(195, 100%, 90%);
-// 	border-top-left-radius: ${props => (!props.topLeft && !props.left ? 0 : "6px")};
-// 	border-top-right-radius: ${props => (!props.topRight && !props.right ? 0 : "6px")};
-// 	border-bottom-left-radius: ${props => (!props.bottomLeft && !props.left ? 0 : "6px")};
-// 	border-bottom-right-radius: ${props => (!props.bottomRight && !props.right ? 0 : "6px")};
-// `
-
 const Center = styled.div`
 	display: flex;
 	flex-direction: row;
@@ -26,11 +17,19 @@ const Content = styled.div`
 const Relative = styled.div`
 	position: relative;
 `
+const RoundedSelection = styled.span`
+	display: inline-block;
+	background-color: ${props => (!props.active ? "hsl(0, 0%, 92.5%)" : "hsl(195, 100%, 92.5%)")};
+	border-top-left-radius: ${props => (!props.rounded.includes("tl") && !props.rounded.includes("l") ? 0 : "2px")};
+	border-top-right-radius: ${props => (!props.rounded.includes("tr") && !props.rounded.includes("r") ? 0 : "2px")};
+	border-bottom-left-radius: ${props => (!props.rounded.includes("bl") && !props.rounded.includes("l") ? 0 : "2px")};
+	border-bottom-right-radius: ${props => (!props.rounded.includes("br") && !props.rounded.includes("r") ? 0 : "2px")};
+`
 const Cursor = styled.span`
 	position: absolute;
 	top: 0;
 	bottom: 0;
-	border-right: 2px solid hsl(195, 100%, 45%);
+	border-right: ${props => (!props.active ? "none" : "2px solid hsl(195, 100%, 45%)")};
 `
 
 export default function App() {
@@ -42,8 +41,8 @@ export default function App() {
 
 					{/* prettier-ignore */}
 					<Relative>
-						Lorem ipsum dolor
-						<Cursor />{" "}
+						Lorem ipsum <RoundedSelection active rounded={["l", "r"]}>dolor</RoundedSelection>
+						<Cursor active />{" "}
 						sit amet.
 					</Relative>
 
@@ -53,51 +52,3 @@ export default function App() {
 		</Center>
 	)
 }
-
-// <Relative>
-// 	<AbsoluteSelectionWrapper>
-// 		<HiddenSelection>Occaecat ad&nbsp;</HiddenSelection>
-// 		<ActiveSelection topLeft topRight bottomRight>
-// 			officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
-// 		</ActiveSelection>
-// 	</AbsoluteSelectionWrapper>
-// 	Occaecat ad officia ullamco occaecat do dolor sit enim culpa eiusmod ipsum. Culpa anim pariatur labore&nbsp;
-// </Relative>
-// <Relative>
-// 	<AbsoluteSelectionWrapper>
-// 		<ActiveSelection topLeft>
-// 			ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum.
-// 			Proident&nbsp;
-// 		</ActiveSelection>
-// 	</AbsoluteSelectionWrapper>
-// 	ullamco irure. Excepteur consectetur aliqua voluptate magna officia ad elit proident cillum. Proident&nbsp;
-// </Relative>
-// <Relative>
-// 	<AbsoluteSelectionWrapper>
-// 		<ActiveSelection topRight>
-// 			nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
-// 			officia&nbsp;
-// 		</ActiveSelection>
-// 	</AbsoluteSelectionWrapper>
-// 	nostrud ex ex sit reprehenderit. Et ngure culpa tempor amet Lorem non irure. Do sit pariatur duis
-// 	officia&nbsp;
-// </Relative>
-// <Relative>
-// 	<AbsoluteSelectionWrapper>
-// 		<ActiveSelection right>
-// 			fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
-// 			minim&nbsp;
-// 		</ActiveSelection>
-// 	</AbsoluteSelectionWrapper>
-// 	fugiat incididunt nostrud qui quis pariatur esse occaecat. Cillum aliquip cupidatat occaecat cillum
-// 	minim&nbsp;
-// </Relative>
-// <Relative>
-// 	<AbsoluteSelectionWrapper>
-// 		<ActiveSelection bottomLeft bottomRight>
-// 			laborum non aliqua exercitation sunt ad mollit&nbsp;
-// 		</ActiveSelection>
-// 		<HiddenSelection>pariatur ea.&nbsp;</HiddenSelection>
-// 	</AbsoluteSelectionWrapper>
-// 	laborum non aliqua exercitation sunt ad mollit pariatur ea.&nbsp;
-// </Relative>
