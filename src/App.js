ed8678b3f1679cb73fdf1509c5ec9bcd3635bf81
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-09 14:24:52 +0900

    Added keyboard properties and events

--- src/App.js
+++ src/App.js
@@ -28,6 +28,25 @@ const Content = styled.div`
 	max-width: ${rem(768)};
 `
 
+// 	// console.log(document.caretRangeFromPoint(e.clientX, e.clientY))
+// 	const range = document.caretRangeFromPoint(e.clientX, e.clientY)
+// 	if (range.startContainer.nodeType === Node.TEXT_NODE) {
+// 		// console.log(range.startContainer.nodeValue.slice(0, range.startOffset))
+// 		setPos(range.startOffset)
+// 	}
+// }}
+// onKeyDown={e => {
+// 	if (e.key === "ArrowLeft") {
+// 		setPos(pos - 1 < 0 ? str.length : pos - 1)
+// 	} else if (e.key === "ArrowRight") {
+// 		setPos(pos + 1 <= str.length ? pos + 1 : 0)
+// 	}
+
+const SHIFT = "shift"
+const CTRL = "ctrl"
+const ALT = "alt"
+const META = "meta"
+
 export default function App() {
 	const articleRef = React.useRef(null)
 
@@ -43,6 +62,30 @@ export default function App() {
 			pointerUp() {
 				state.pointer.down = false
 			},
+			keyDown(type) {
+				const match = {
+					[SHIFT]: true,
+					[CTRL]: true,
+					[ALT]: true,
+					[META]: true,
+				}[type]
+				if (!match) {
+					throw new Error(`keyDown: no such type; type=${type}`)
+				}
+				state.keyboard[type] = true
+			},
+			keyUp(type) {
+				const match = {
+					[SHIFT]: true,
+					[CTRL]: true,
+					[ALT]: true,
+					[META]: true,
+				}[type]
+				if (!match) {
+					throw new Error(`keyUp: no such type; type=${type}`)
+				}
+				state.keyboard[type] = false
+			},
 			focus() {
 				state.activeElement = true
 			},
@@ -56,15 +99,21 @@ export default function App() {
 				x: 0,
 				y: 0,
 			},
+			keyboard: {
+				[SHIFT]: false,
+				[CTRL]: false,
+				[ALT]: false,
+				[META]: false,
+			},
 			activeElement: false,
 			document: {
 				content: "Hello, world!",
 				selection: {
 					start: {
-						// ...
+						offset: 0,
 					},
 					end: {
-						// ...
+						offset: 0,
 					},
 				},
 			},
@@ -79,7 +128,10 @@ export default function App() {
 				<Content>
 					<article
 						ref={articleRef}
-						style={{ fontSize: 19 }}
+						style={{
+							fontSize: 19,
+							outline: "none",
+						}}
 						onPointerMove={e => {
 							dispatch.pointerMove({ x: e.clientX, y: e.clientY })
 						}}
@@ -89,6 +141,29 @@ export default function App() {
 						onPointerUp={e => {
 							dispatch.pointerUp()
 						}}
+						onKeyDown={e => {
+							const type = {
+								Shift: SHIFT,
+								Control: CTRL,
+								Alt: ALT,
+								Meta: META,
+							}[e.key]
+							if (type) {
+								dispatch.keyDown(type)
+							}
+						}}
+						onKeyUp={e => {
+							const type = {
+								Shift: SHIFT,
+								Control: CTRL,
+								Alt: ALT,
+								Meta: META,
+							}[e.key]
+							if (type) {
+								dispatch.keyUp(type)
+							}
+						}}
+						tabIndex={0}
 					>
 						{/**/}
 
