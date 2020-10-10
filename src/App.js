e3b9bd9ee0dc800a2c407b88150d2c64d4b74418
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-10 11:29:22 +0900

    Extracted useEditor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useCallback, useLayoutEffect, useRef } from "react"
 import styled, { css } from "styled-components"
 
 import rem from "rem"
-import useMethods from "use-methods"
+import useEditor from "useEditor"
 
 // const AbsoluteCaret = styled.span`
 // 	margin-left: ${rem(-1)};
@@ -25,152 +25,6 @@ function getKeyDownModKeys(e) {
 		metaKey: e.metaKey,
 	}
 	return modKeys
-}
-
-const methods = state => ({
-	resize(layout) {
-		state.layout = layout
-	},
-
-	pointerMove({ coords, range }) {
-		state.pointer.x = coords.x
-		state.pointer.y = coords.y
-		// state.pointer.down = true
-
-		if (!state.pointer.down) {
-			// No-op
-			return
-		} else if (coords.y < state.layout.top || coords.y > state.layout.bottom) {
-			// No-op
-			return
-		} else if (coords.x < state.layout.left || coords.x > state.layout.right) {
-			// No-op
-			return
-		}
-		state.document.range.start = range.start
-		// state.document.range.end = range.end
-	},
-	pointerDown({ coords, range }) {
-		state.pointer.x = coords.x
-		state.pointer.y = coords.y
-		state.pointer.down = true
-
-		state.document.range.start = range.start
-		state.document.range.end = range.end
-	},
-	pointerUp() {
-		state.pointer.down = false
-	},
-
-	// setRangeComputed(computed) {
-	// 	state.document.range.__computed = computed
-	// },
-	setComputedOpenRange(computed) {
-		state.document.range.__computed = computed
-	},
-
-	focus() {
-		state.document.activeElement = true
-	},
-	blur() {
-		state.document.activeElement = false
-	},
-
-	// dir=up
-	// dir=right
-	// dir=down
-	// dir=left
-	// boundary=rune
-	// boundary=word
-	// boundary=line
-	// boundary=node
-	// boundary=page
-	//
-	keyDownArrowUp(modKeys) {
-		// TODO
-	},
-	keyDownArrowDown(modKeys) {
-		// TODO
-	},
-	// keyDownArrowLeft(modKeys) {
-	// 	// state.document.range.direction = "start"
-	// 	if (state.document.range.start > 0) {
-	// 		state.document.range.start--
-	// 		state.document.range.end--
-	// 	}
-	// },
-	// keyDownArrowRight(modKeys) {
-	// 	// state.document.range.direction = "start"
-	// 	if (state.document.range.end < state.document.content.length) {
-	// 		state.document.range.start++
-	// 		state.document.range.end++
-	// 	}
-	// },
-	keyDownArrowLeft(modKeys) {
-		if (!modKeys.shiftKey) {
-			// state.document.range.direction = "none"
-			if (state.document.range.start > 0) {
-				state.document.range.start--
-				state.document.range.end--
-			}
-		} else {
-			// state.document.range.direction = "backwards"
-			if (state.document.range.start > 0) {
-				state.document.range.start--
-			}
-		}
-	},
-	keyDownArrowRight(modKeys) {
-		if (!modKeys.shiftKey) {
-			// state.document.range.direction = "none"
-			if (state.document.range.end < state.document.content.length) {
-				state.document.range.start++
-				state.document.range.end++
-			}
-		} else {
-			// state.document.range.direction = "forwards"
-			if (state.document.range.end < state.document.content.length) {
-				state.document.range.end++
-			}
-		}
-	},
-
-	// keyDownBackspace() {
-	// 	// ...
-	// },
-	// keyDownForwardBackspace() {
-	// 	// ...
-	// },
-	// keyDownCharacter(character) {
-	// 	// ...
-	// },
-})
-
-const initialState = {
-	layout: {
-		top: 0,
-		right: 0,
-		bottom: 0,
-		left: 0,
-	},
-	pointer: {
-		x: 0,
-		y: 0,
-		down: false,
-	},
-	document: {
-		activeElement: false,
-		content: "Hello, world!",
-		range: {
-			// direction: "start", // TODO
-			start: 0,
-			end: 0,
-			__computed: {
-				left: 0,
-				right: 0,
-			},
-		},
-	},
 }
 
 // Ex:
@@ -198,7 +52,7 @@ export default function App() {
 	const articleRef = useRef(null)
 	const measureRef = useRef(null)
 
-	const [state, dispatch] = useMethods(methods, initialState)
+	const [state, dispatch] = useEditor("Hello, world!")
 
 	React.useLayoutEffect(() => {
 		const handler = e => {
@@ -257,9 +111,9 @@ export default function App() {
 			<style>
 				{css`
 					html {
-						--selection-color: hsla(200, 100%, 90%, 0.9);
-						--inactive-selection-color: hsla(200, 0%, 90%, 0.75);
-						--caret-color: hsla(200, 100%, 50%, 1);
+						--selection-color: hsla(210, 100%, 90%, 1);
+						--inactive-selection-color: hsla(210, 0%, 90%, 0.75);
+						--caret-color: hsla(210, 100%, 45%, 1);
 					}
 				`}
 			</style>
@@ -379,7 +233,7 @@ export default function App() {
 										bottom: 0,
 										left: state.document.range.__computed.end,
 										borderRight: `${rem(2)} solid var(--caret-color)`,
-										borderRadius: 9999,
+										/* borderRadius: 9999, */
 										zIndex: 20,
 										pointerEvents: "none",
 										userSelect: "none",
@@ -408,7 +262,7 @@ export default function App() {
 										backgroundColor: !state.document.activeElement
 											? "var(--inactive-selection-color)"
 											: "var(--selection-color)",
-										borderRadius: rem(2),
+										/* borderRadius: rem(3), */
 										zIndex: 0,
 										pointerEvents: "none",
 										userSelect: "none",
---
+++ src/useEditor.js
@@ -0,0 +1,162 @@
+import useMethods from "use-methods"
+
+const methods = state => ({
+	resize(layout) {
+		state.layout = layout
+	},
+
+	pointerMove({ coords, range }) {
+		state.pointer.x = coords.x
+		state.pointer.y = coords.y
+		// state.pointer.down = true
+
+		if (!state.pointer.down) {
+			// No-op
+			return
+		} else if (coords.y < state.layout.top || coords.y > state.layout.bottom) {
+			// No-op
+			return
+		} else if (coords.x < state.layout.left || coords.x > state.layout.right) {
+			// No-op
+			return
+		}
+		state.document.range.start = range.start
+		// state.document.range.end = range.end
+	},
+	pointerDown({ coords, range }) {
+		state.pointer.x = coords.x
+		state.pointer.y = coords.y
+		state.pointer.down = true
+
+		state.document.range.start = range.start
+		state.document.range.end = range.end
+	},
+	pointerUp() {
+		state.pointer.down = false
+	},
+
+	// setRangeComputed(computed) {
+	// 	state.document.range.__computed = computed
+	// },
+	setComputedOpenRange(computed) {
+		state.document.range.__computed = computed
+	},
+
+	focus() {
+		state.document.activeElement = true
+	},
+	blur() {
+		state.document.activeElement = false
+	},
+
+	// dir=up
+	// dir=right
+	// dir=down
+	// dir=left
+	// boundary=rune
+	// boundary=word
+	// boundary=line
+	// boundary=node
+	// boundary=page
+	//
+	keyDownArrowUp(modKeys) {
+		// TODO
+	},
+	keyDownArrowDown(modKeys) {
+		// TODO
+	},
+	// keyDownArrowLeft(modKeys) {
+	// 	// state.document.range.direction = "start"
+	// 	if (state.document.range.start > 0) {
+	// 		state.document.range.start--
+	// 		state.document.range.end--
+	// 	}
+	// },
+	// keyDownArrowRight(modKeys) {
+	// 	// state.document.range.direction = "start"
+	// 	if (state.document.range.end < state.document.content.length) {
+	// 		state.document.range.start++
+	// 		state.document.range.end++
+	// 	}
+	// },
+	keyDownArrowLeft(modKeys) {
+		if (!modKeys.shiftKey) {
+			// state.document.range.direction = "none"
+			if (state.document.range.start > 0) {
+				state.document.range.start--
+				state.document.range.end--
+			}
+		} else {
+			// state.document.range.direction = "backwards"
+			if (state.document.range.start > 0) {
+				state.document.range.start--
+			}
+		}
+	},
+	keyDownArrowRight(modKeys) {
+		if (!modKeys.shiftKey) {
+			// state.document.range.direction = "none"
+			if (state.document.range.end < state.document.content.length) {
+				state.document.range.start++
+				state.document.range.end++
+			}
+		} else {
+			// state.document.range.direction = "forwards"
+			if (state.document.range.end < state.document.content.length) {
+				state.document.range.end++
+			}
+		}
+	},
+
+	// keyDownBackspace() {
+	// 	// ...
+	// },
+	// keyDownForwardBackspace() {
+	// 	// ...
+	// },
+	// keyDownCharacter(character) {
+	// 	// ...
+	// },
+})
+
+const initialState = {
+	layout: {
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	},
+	pointer: {
+		x: 0,
+		y: 0,
+		down: false,
+	},
+	document: {
+		activeElement: false,
+		content: "Hello, world!",
+		range: {
+			// direction: "start", // TODO
+			start: 0,
+			end: 0,
+			__computed: {
+				left: 0,
+				right: 0,
+			},
+		},
+	},
+}
+
+function init(initialValue) {
+	const state = {
+		...initialState,
+		document: {
+			...initialState.document,
+			content: initialValue,
+		},
+	}
+	return state
+}
+
+export default function useEditor(initialValue) {
+	return useMethods(methods, null, () => init(initialValue))
+}
